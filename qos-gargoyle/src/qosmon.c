196648ca9f722caa85652750e709d3b09e0d1a35
ilxp <lixp@live.com>
2026-09-16 13:22:11 +0800

    Update qosmon.c
    修复在lede的6.18编译错误

--- qos-gargoyle/src/qosmon.c
+++ qos-gargoyle/src/qosmon.c
@@ -424,7 +424,7 @@ char pr_pack( void *buf, int cc, struct sockaddr_storage *from )
 
 //These variables referenced but not used by the tc code we link to.
 int filter_ifindex;
-int use_iec = 0;
+extern int use_iec;   // 由 iproute2 的 utils.o 提供
 
 
 int print_class(struct nlmsghdr *n, void *arg)
